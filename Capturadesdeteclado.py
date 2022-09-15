56b42daa3a1afad6a7312cf385a229bfa93addcf
Reyner D Paz <ing.reyner.de.paz@gmail.com>
2022-09-14 19:18:11 -0500

    tercer cambio

--- Capturadesdeteclado.py
+++ Capturadesdeteclado.py
@@ -3,7 +3,9 @@ n1=input('Ingrese Numero 1 ')
 n2=input('Ingrese Numero 2 ')
 suma=float(n1)+float(n2)
 print('La suma es:', suma)
+print('La suma es:', suma)
 
+print('La suma es:', suma)
 
 #int
 #float
